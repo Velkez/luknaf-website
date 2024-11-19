534e6d99716f946dc8c44d9b64aeb3529daf88a7
Elkin Vasquez <velkezdev@gmail.com>
2024-11-18 23:32:10 -0500

    setting up react router dom

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,3 +1,5 @@
+// será util tomar un curso de React. Carajo
+
 export default function Header(){
     return(
         <header className="text-center py-4">Hola este es un ejemplo de Header</header>

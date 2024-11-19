3d4dd2a27dc54464ebf003924b9003f6a09069e3
Elkin Vasquez <velkezdev@gmail.com>
2024-11-18 23:29:16 -0500

    setting up react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import { useState } from 'react'
+import MainPage from './pages/MainPage';
+import { Routes, Route } from "react-router-dom";
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className='App'>
+      <Routes>
+        <Route path='/' element={<MainPage/>}/>
+      </Routes>
+    </div>
   )
 }
 
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,5 @@
+export default function Header(){
+    return(
+        <header className="text-center py-4">Hola este es un ejemplo de Header</header>
+    );
+}
